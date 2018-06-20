55e63d017ccf0311b24042542411947a758647a2
pyoung21 <30935238+pyoung21@users.noreply.github.com>
2018-06-20 02:57:25 +0200

    Update maths.c

--- src/maths.c
+++ src/maths.c
@@ -10,12 +10,12 @@
 
 float my_sqrt(float n)
 {
-		int i = 0x5f3759df - ((*(int*)&n) >> 1);
-		float y = *(float*)&i;
-		y *= (1.5f - 0.5f * n * y * y);
-		y *= (1.5f - 0.5f * n * y * y);
-		y *= (1.5f - 0.5f * n * y * y);
-		return ((float)(y * n));
+	int i = 0x5f3759df - ((*(int*)&n) >> 1);
+	float y = *(float*)&i;
+	y *= (1.5f - 0.5f * n * y * y);
+	y *= (1.5f - 0.5f * n * y * y);
+	y *= (1.5f - 0.5f * n * y * y);
+	return ((float)(y * n));
 }
 
 float my_pow(float f, int n)
@@ -30,10 +30,10 @@ float my_pow(float f, int n)
 
 int get_dist(const t_vertex *v1, const t_vertex *v2)
 {
-		float x2 = my_pow(v2->x - v1->x, 2);
-		float y2 = my_pow(v2->y - v1->y, 2);
-		float n = my_sqrt(x2 + y2);
-		int i = (int)n;
-		int dist = (n - (float)i) > 0.49f ? i + 1 : i;
-		return (dist);
+	float x2 = my_pow(v2->x - v1->x, 2);
+	float y2 = my_pow(v2->y - v1->y, 2);
+	float n = my_sqrt(x2 + y2);
+	int i = (int)n;
+	int dist = (n - (float)i) > 0.49f ? i + 1 : i;
+	return (dist);
 }
